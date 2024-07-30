8c6980eee9c26e7ca9b691fa4cb781f388aa571a
Daniel Rizk <106986996+DanielRizk@users.noreply.github.com>
2024-07-30 15:31:42 +0200

    Update init_data.sh

--- init_data.sh
+++ init_data.sh
@@ -1,8 +1,19 @@
 # WWTP/init_data.sh
 #!/bin/bash
 
+# create data directories 
+mkdir node-red
+mkdir node-red/data
+mkdir grafana
+mkdir grafana/data
+
 # Copy initial Node-RED data
 cp -r ./initial_data/node-red/data/* ./node-red/data/
 
 # Copy initial Grafana data
 cp -r ./initial_data/grafana/data/* ./grafana/data/
+
+# create a virtual env
+cd Server
+python3 -m venv venv
+cd ..
